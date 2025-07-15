12b7837667b71018354b4d81d720db33c9386e50
MJcoder20 <marahjouda51@gmail.com>
2025-07-15 12:08:23 +0300

    updated

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Code usage instructions
+Run the main file and enter data as instructed in the about menu item 
+that appears when running the app.
+
+# Code distribution among team members
 MJCoder20(Marah Jouda)'s code includes main.py, the data link, the first three functions in the calc.py file, and the menu bar connections.
 Alaa-Abo-Auon's code includes the calc.py file in the processing folder.
 Afnan97Sh's code includes the visualize.py file in the visualization folder.
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from openpyxl import Workbook
 import openpyxl
 import tkinter as tk
@@ -5,6 +7,18 @@ from tkinter import messagebox
 
 from processing.calc import *
 from visualization.visualize import *
+
+
+def validate_date():
+    date = date_entry.get()
+
+    try:
+        # Attempt to parse the date in the desired format (e.g., DD/MM/YYYY)
+        valid_date = datetime.strptime(date, "%d/%m/%Y")
+        return valid_date
+    except ValueError:
+        date_entry.delete(0, tk.END)
+        messagebox.showerror("Error", "Invalid date format! Please use DD/MM/YYYY.")
 
 
 def submit_data():
@@ -30,16 +44,23 @@ def submit_data():
             # Add headers if creating a new file
             sheet.append(['Date', 'Region', "Martyr Count", 'Injured Count', 'Damaged Homes Count', 'Attack Type'])
 
+        regions = ['Shigaeya', 'Baithanoun', 'Baitlahya', 'Nusairat', 'Deir-El-Balah', 'Zawaida', 'Khanyunis',
+                   'Rafah', 'Gaza-Jalaa', 'Gaza-El_Saha', 'Gaza-Tuffah', 'Gaza-Rimal', 'Gaza-Tal_El_Hawaa',
+                   'Gaza-El_Sheikh_Redawan', 'Gaza-El_Nasser']
         # Append user data
-        sheet.append([date, region, martyr_count, injured_count, damaged_homes_count, attack_type])
-        workbook.save("data/Book1.xlsx")
-        messagebox.showinfo("Success", "Data saved successfully!")
-        date_entry.delete(0, tk.END)
-        region_entry.delete(0, tk.END)
-        martyr_count_entry.delete(0, tk.END)
-        injured_count_entry.delete(0, tk.END)
-        damaged_homes_count_entry.delete(0, tk.END)
-        attack_type_entry.delete(0, tk.END)
+        if validate_date() and region and (region in regions) and martyr_count.isdigit() and injured_count.isdigit() \
+                and damaged_homes_count.isdigit() and attack_type and attack_type in \
+                ['Ground', 'Arial', 'Seaborn']:
+            sheet.append([validate_date(), region, martyr_count, injured_count, damaged_homes_count, attack_type])
+            workbook.save("data/Book1.xlsx")
+            messagebox.showinfo("Success", "Data saved successfully!")
+            date_entry.delete(0, tk.END)
+            region_entry.delete(0, tk.END)
+            martyr_count_entry.delete(0, tk.END)
+            injured_count_entry.delete(0, tk.END)
+            damaged_homes_count_entry.delete(0, tk.END)
+            attack_type_entry.delete(0, tk.END)
+
     except Exception as e:
         messagebox.showerror("Error", f"An error occurred: {e}")
 
@@ -49,37 +70,37 @@ def attacks_window():
     root2.title("Attacks' Statistics")
     root2.geometry("400x600")
     root2.config(bg="light blue")
-    tk.Label(root2, text='Total Martyr Count: ').grid(row=7, column=0, padx=15, pady=10)
+    tk.Label(root2, text='Total Martyr Count: ', font=("Helvetica", 9, "bold")).grid(row=7, column=0, padx=15, pady=10)
     tk.Label(root2, text=total_martyr_count()).grid(row=7, column=1, padx=15, pady=10)
 
-    tk.Label(root2, text='Total Injured Count: ').grid(row=8, column=0, padx=15, pady=10)
+    tk.Label(root2, text='Total Injured Count: ', font=("Helvetica", 9, "bold")).grid(row=8, column=0, padx=15, pady=10)
     tk.Label(root2, text=total_injured_count()).grid(row=8, column=1, padx=15, pady=10)
 
-    tk.Label(root2, text='Total Victim Count: ').grid(row=9, column=0, padx=15, pady=10)
+    tk.Label(root2, text='Total Victim Count: ', font=("Helvetica", 9, "bold")).grid(row=9, column=0, padx=15, pady=10)
     tk.Label(root2, text=total_martyr_injured_count()).grid(row=9, column=1, padx=15, pady=10)
 
-    tk.Label(root2, text='Most Damaged Region: ').grid(row=10, column=0, padx=15, pady=10)
+    tk.Label(root2, text='Most Damaged Region: ', font=("Helvetica", 9, "bold")).grid(row=10, column=0, padx=15, pady=10)
     tk.Label(root2, text=most_damaged_region()).grid(row=10, column=1, padx=15, pady=10)
 
-    tk.Label(root2, text='Least Damaged Region: ').grid(row=11, column=0, padx=15, pady=10)
+    tk.Label(root2, text='Least Damaged Region: ', font=("Helvetica", 9, "bold")).grid(row=11, column=0, padx=15, pady=10)
     tk.Label(root2, text=least_damaged_region()).grid(row=11, column=1, padx=15, pady=10)
 
-    tk.Label(root2, text='Highest Victims Date: ').grid(row=12, column=0, padx=15, pady=10)
+    tk.Label(root2, text='Highest Victims Date: ', font=("Helvetica", 9, "bold")).grid(row=12, column=0, padx=15, pady=10)
     tk.Label(root2, text=highest_victims_date()).grid(row=12, column=1, padx=15, pady=10)
 
-    tk.Label(root2, text='Lowest Victims Date: ').grid(row=13, column=0, padx=15, pady=10)
+    tk.Label(root2, text='Lowest Victims Date: ', font=("Helvetica", 9, "bold")).grid(row=13, column=0, padx=15, pady=10)
     tk.Label(root2, text=lowest_victims_date()).grid(row=13, column=1, padx=15, pady=10)
 
-    tk.Label(root2, text='Attack Type Count: ').grid(row=14, column=0, padx=15, pady=10)
+    tk.Label(root2, text='Attack Type Count: ', font=("Helvetica", 9, "bold")).grid(row=14, column=0, padx=15, pady=10)
     tk.Label(root2, text=attack_type_count()).grid(row=14, column=1, padx=15, pady=10)
 
     regions = df['Region'].unique()
-    tk.Label(root2, text='Martyr Percentage By Region: ').grid(row=15, column=0, padx=5, pady=10)
-    i=0
+    tk.Label(root2, text='Martyr Percentage By Region: ', font=("Helvetica", 9, "bold")).grid(row=15, column=0, padx=5, pady=10)
+    i = 0
     for region in regions:
-         tk.Label(root2, text=region).grid(row=16+i, column=0, padx=10, pady=5)
-         tk.Label(root2, text=martyr_percentage_by_region(region)).grid(row=16+i, column=1, padx=10, pady=5)
-         i = i+1
+        tk.Label(root2, text=region).grid(row=16+i, column=0, padx=10, pady=5)
+        tk.Label(root2, text=martyr_percentage_by_region(region)).grid(row=16+i, column=1, padx=10, pady=5)
+        i = i+1
 
     # Allow the window to resize automatically
     root2.update_idletasks()
@@ -94,10 +115,13 @@ def about_usage():
     root3.title("Usage Instructions")
     root3.geometry("100x100")
     root3.config(bg="light gray")
-    tk.Label(root3, text='Basic Usage guidelines: ', bg="skyblue").grid(row=7, column=0, padx=15, pady=10)
+    tk.Label(root3, text='Basic Usage guidelines ', font=("Helvetica", 12), bg="skyblue").grid(row=7, column=0, padx=15, pady=10)
     tk.Label(root3, text="1. Enter data into all the fields or else none of the data will be stored."
-                         "\n2. Recently submitted data won't appear in the statistics or the visuals windows "
-                         "unless you rerun the app.").grid(row=8, column=0, padx=15, pady=10)
+                         "\n\n2. Make sure to enter camel case data(eg., Gaza-Rimal, Zawaida, Gaza-Tal_El_Hawaa, etc.)."
+                         "\n\n3. Recently submitted data won't appear in the statistics or the visuals windows, "
+                         "so be sure to rerun the app if you have added any new information."
+             , font=("Helvetica", 10))\
+        .grid(row=8, column=0, padx=15, pady=10)
 
     # Allow the window to resize automatically
     root3.update_idletasks()
@@ -119,7 +143,7 @@ root.config(menu=menubar)  # Associate the menu bar with the window
 
 statsMenu = tk.Menu(menubar, tearoff=0)  # tearoff=0 removes the dashed line
 menubar.add_cascade(label="Statistics", menu=statsMenu)  # Add stats menu to the menubar
-statsMenu.add_command(label="Attacks Data", command=attacks_window) # creates a new window with the calculated stats
+statsMenu.add_command(label="Attacks Data", command=attacks_window)  # creates a new window with the calculated stats
 
 visualizeMenu = tk.Menu(menubar, tearoff=0)
 menubar.add_cascade(label="Visualization", menu=visualizeMenu)  # Add visualize menu to the menubar
@@ -131,31 +155,31 @@ visualizeMenu.add_command(label="Days with the highest martyr counts", command=m
 visualizeMenu.add_command(label="Heatmap of Damaged Homes by Region and Attack Type", command=heatmap)
 
 aboutMenu = tk.Menu(menubar, tearoff=0)
-menubar.add_cascade(label="About", menu=aboutMenu) # Add about menu to explain our app's functionality
+menubar.add_cascade(label="About", menu=aboutMenu)  # Add about menu to explain our app's functionality
 aboutMenu.add_command(label="Usage", command=about_usage)
 
 # Labels and Entry fields
-tk.Label(root, text="Date:").grid(row=0, column=0, padx=15, pady=10)
+tk.Label(root, text="Date (DD/MM/YYYY)").grid(row=0, column=0, padx=15, pady=10)
 date_entry = tk.Entry(root)
 date_entry.grid(row=0, column=2, padx=15, pady=10)
 
-tk.Label(root, text="Region:").grid(row=1, column=0, padx=15, pady=10)
+tk.Label(root, text="Region").grid(row=1, column=0, padx=15, pady=10)
 region_entry = tk.Entry(root)
 region_entry.grid(row=1, column=2, padx=15, pady=10)
 
-tk.Label(root, text="Martyr Count:").grid(row=2, column=0, padx=15, pady=10)
+tk.Label(root, text="Martyr Count").grid(row=2, column=0, padx=15, pady=10)
 martyr_count_entry = tk.Entry(root)
 martyr_count_entry.grid(row=2, column=2, padx=15, pady=10)
 
-tk.Label(root, text="Injured Count:").grid(row=3, column=0, padx=15, pady=10)
+tk.Label(root, text="Injured Count").grid(row=3, column=0, padx=15, pady=10)
 injured_count_entry = tk.Entry(root)
 injured_count_entry.grid(row=3, column=2, padx=15, pady=10)
 
-tk.Label(root, text="Damaged Homes Count:").grid(row=4, column=0, padx=15, pady=10)
+tk.Label(root, text="Damaged Homes Count").grid(row=4, column=0, padx=15, pady=10)
 damaged_homes_count_entry = tk.Entry(root)
 damaged_homes_count_entry.grid(row=4, column=2, padx=15, pady=10)
 
-tk.Label(root, text="Attack Type:").grid(row=5, column=0, padx=15, pady=10)
+tk.Label(root, text="Attack Type").grid(row=5, column=0, padx=15, pady=10)
 attack_type_entry = tk.Entry(root)
 attack_type_entry.grid(row=5, column=2, padx=15, pady=10)
 
